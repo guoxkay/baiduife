2aca13cb75b36c70b772a721fa2177fa58487fb8
guoxkay <guoxkay@qq.com>
2015-10-12 21:38:59 +0800

    ++

--- summer/2048/js/2048.js
+++ summer/2048/js/2048.js
@@ -1,13 +1,98 @@
 var game = (function(){
 	var l = localStorage;
-	var highScore = l.get("score");//读取最高分
+	var highScore = l.get("score") || 0;//读取最高分
 	var score = 0;//分数
 	return {
 		//开始游戏
 		beginGame : function(){
-			var xA = Math.floor(Math.random() * 4);
-			var yA = Math.floor(Math.random() * 4);
-			$("#cell_" + yA + "_" + xA).
+			var x1 = Math.floor(Math.random() * 4);
+			var y1 = Math.floor(Math.random() * 4);
+			$("#cell_" + y1 + "_" + x1).addClass("n2").text("2");
+			var x2 = Math.floor(Math.random() * 4);
+			var y2 = Math.floor(Math.random() * 4);
+			while (x2 === x1 && y2 === y1){
+				x2 = Math.floor(Math.random() * 4);
+				y2 = Math.floor(Math.random() * 4);
+			}
+			$("#cell_" + y2 + "_" + x2).addClass("n2").text("2");
+		},
+		//随机产生2和4
+		randomEmerge : function(){
+			var empty = [];
+			for (var i = 0;i < 4;i++){
+				for (var k = 0;k < 4;k++){
+					if ($("#cell_" + i + "_" + k).className.length = 1){
+						empty.push($("#cell_" + i + "_" + k))
+					}
+				}
+			}
+			if (empty.length = 0){
+				return
+			}
+			else{
+				var number = Math.floor(Math.random() * 2);
+				if (number === 0){
+					empty[Math.floor(Math.random() * empty.length)].addClass("n2").text("2")
+				}
+				else{
+					empty[Math.floor(Math.random() * empty.length)].addClass("n4").text("4")
+				}
+			}
+		},
+		//判断是否成功
+		isWin : function(){
+			var allCell = $(".cell");
+			for (var i = 0;i < allCell.length;i++){
+				if (allCell[i].text() === "2048"){
+					$("document").unbind("keydown");
+					$(".win p").text("您成功了");
+					$(".win").fadeIn(500);
+					if(score > highScore){
+						l.set("score",score)
+					}
+					$("document").click(function(){location.reload()});
+					return true
+				}
+			}
+			return false
+		},
+		//判断是否失败
+		isFail : function(){
+			var allCell = $(".cell");
+			for (var i = 0;i < allCell.length;i++){
+				if (allCell[i].className.length === 1){
+					return false
+				}
+			}
+			for (var i = 0;i < 4;i++){
+				for (var k = 0;k < 4;k++){
+					var cell = $("#cell_" + i + "_" + k);
+					if (cell.text() === $("#cell_" + (i+1) + "_" + k).text()){
+						return false
+					}
+					if (cell.text() === $("#cell_" + i + "_" + (k+1)).text()){
+						return false
+					}
+					if (cell.text() === $("#cell_" + (i-1) + "_" + k).text()){
+						return false
+					}
+					if (cell.text() === $("#cell_" + i + "_" + (k-1)).text()){
+						return false
+					}
+				}
+			}
+			$("document").unbind("keydown");
+			$(".win p").text("游戏结束");
+			$(".win").fadeIn(500);
+			if(score > highScore){
+				l.set("score",score)
+			}
+			$("document").click(function(){location.reload()});
+			return true
+		},
+		//左
+		leftDown : function(){
+			
 		}
 	}
 
